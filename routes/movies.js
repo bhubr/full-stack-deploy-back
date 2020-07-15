69e76b8a007cfd6e52b77c8336d988bdd31dcab4
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-07-15 14:02:12 +0200

    Add movie creation endpoint

--- routes/movies.js
+++ routes/movies.js
@@ -5,7 +5,23 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   connection.query('SELECT * from movie', (err, movies) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).json({ error: err.message })
+    }
     res.json(movies)
+  })
+})
+
+router.post('/', (req, res) => {
+  const sqlInsert = 'INSERT INTO movie SET ?'
+  connection.query(sqlInsert, [req.body], (err, stats) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).json({ error: err.message })
+    }
+    const newMovie = { ...req.body, id: stats.insertId }
+    return res.status(201).json(newMovie)
   })
 })
 
